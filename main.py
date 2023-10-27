42978d5503db26a566b9f8a9f84c5510f2d89958
Travis Webb <me@traviswebb.com>
2023-10-27 17:19:55 -0400

    first pass

---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+
+
+def get_schema(filename):
+    schema = []
+    keys = ['geom']
+
+    with open(filename) as file:
+        # TODO add check for FeatureCollection
+        # if file.length = 1 etc
+
+        for line in file:
+            line_json = json.loads(line)
+            props = line_json['properties']
+
+            for key, value in props.items():
+                if key in keys:
+                    continue
+
+                keys.append(key)
+
+                if isinstance(value, str):
+                    schema.append({ 'name': key, 'value': 'STRING' })
+                elif isinstance(value, int):
+                    schema.append({ 'name': key, 'value': 'INT64' })
+                elif isinstance(value, float):
+                    schema.append({ 'name': key, 'value': 'FLOAT64' })
+
+        schema.append({
+            'name': 'geom',
+            'type': 'GEOGRAPHY'
+        })
+
+        return schema
+
+
+if __name__ == '__main__':
+    import logging
+    import argparse
+
+    logging.getLogger().setLevel(logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', required=True)
+    #parser.add_argument('--required', )
+    args = parser.parse_args()
+
+    print(json.dumps(get_schema(args.filename), indent=4))
